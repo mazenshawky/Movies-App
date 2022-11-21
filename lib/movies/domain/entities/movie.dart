40b3811fa33a32bbf1ecad97593e7ee40183578f
Mazen Shawky <mazenshawqy@gmail.com>
2022-11-21 04:37:02 +0200

    create movie entity

---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,28 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> genreIds;
+  final String overview;
+  final double voteAverage;
+
+  const Movie(
+      {required this.id,
+      required this.title,
+      required this.backdropPath,
+      required this.genreIds,
+      required this.overview,
+      required this.voteAverage});
+
+  @override
+  List<Object?> get props => [
+        id,
+        title,
+        backdropPath,
+        genreIds,
+        overview,
+        voteAverage,
+      ];
+}
