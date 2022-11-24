438ef90c1d8c66b78f3adbd2b06b85c608502c97
Mazen Shawky <mazenshawqy@gmail.com>
2022-11-25 01:40:45 +0200

    recommeded movies domain layer

--- lib/movies/domain/repostiory/base_movies_repository.dart
+++ lib/movies/domain/repostiory/base_movies_repository.dart
@@ -1,7 +1,9 @@
 import 'package:dartz/dartz.dart';
 import 'package:movies_app/movies/domain/entities/movie.dart';
 import 'package:movies_app/movies/domain/entities/movie_details.dart';
+import 'package:movies_app/movies/domain/entities/recommendation.dart';
 import 'package:movies_app/movies/domain/usecases/get_movie_details_usecase.dart';
+import 'package:movies_app/movies/domain/usecases/get_recommendation_usecase.dart';
 
 import '../../../core/error/failure.dart';
 
@@ -14,4 +16,7 @@ abstract class BaseMoviesRepository {
 
   Future<Either<Failure, MovieDetails>> getMovieDetails(
       MovieDetailsParameters parameters);
+
+  Future<Either<Failure, List<Recommendation>>> getRecommendation(
+      RecommendaitonParameters parameters);
 }
